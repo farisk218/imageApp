967db0df5cd3fe5c079c777804c410ce3b65378d
xlmac <xlmac@xlmacs-iMac.local>
2020-08-13 02:36:19 +0530

    Updates

---
+++ precache-manifest.c439815c431ec6fc630367a6695f5bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c58ef641dbb13e74f829e034bdbeaf",
+    "url": "/imageApp/index.html"
+  },
+  {
+    "revision": "78b0789b8662e38a44bb",
+    "url": "/imageApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a84f78a2e0c3e76aecec",
+    "url": "/imageApp/static/js/2.41fe73cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/imageApp/static/js/2.41fe73cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b0789b8662e38a44bb",
+    "url": "/imageApp/static/js/main.e6b60978.chunk.js"
+  },
+  {
+    "revision": "e4316634c53ca7925601",
+    "url": "/imageApp/static/js/runtime-main.8d64e82a.js"
+  }
+]);
